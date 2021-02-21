9fb7317e761198bba77cb888897216b6e3ea624e
Jelena Jovanovic <djurovic.jelena@gmail.com>
2021-02-21 12:24:16 +0100

    Draft functionality layout

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# part1 prefix calculator
+def prefex_calc(input):
+    pass
+
+# part2 infix to prefex
+def infix_to_prefix(input):
+    pass
+
+if __name__ == '__main__':
+    prefix_input= '- / 10 + 1 1 * 1 2'
+    prefex_calc(prefix_input)
