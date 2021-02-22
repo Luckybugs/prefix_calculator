ce600d9609c9b152a1562ed7413826e478a63a86
Jelena Jovanovic <djurovic.jelena@gmail.com>
2021-02-22 14:37:32 +0100

    Recursive solution for infix to prefix conversion

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# part1 prefix calculator
 op = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
@@ -21,42 +22,54 @@ def evaluate_rec(expr_iter):
     return op[elem](op1, op2)
 
 
-pres = {'*': 2, '/': 2, '+': 1, '-': 1, '(': 0, ')': 0}
+# part2 infix to prefex    
+def infix_to_prefix(expr):
+    result = convert_rec(expr.replace(' ', ''))
+    return result.replace('', ' ')
 
+def convert_rec(expr):
 
-# part2 infix to prefex
-def infix_to_prefix(expression):
-    expression = expression.strip().split(' ')
-    stack, res = [], []
-    pop = None
-
-    for e in reversed(expression):
-        if e.isdigit():
-            res.append(e)
-        elif e == ')':
-            stack.append(e)
-        elif e == '(':
-            # remove all up to the closing )
-            pop = stack.pop
-            while pop != ')':
-                pop = stack.pop()
-                if pop != ')':
-                    res.append(pop)
+    # Single element. It can not be split in two parts
+    if len(expr) == 1 and '(' not in expr and ')' not in expr:
+        return expr
+    else:
+        # The first character is a bracket, which means that the first part is expression in parentheses
+        if expr[0] == '(':
+            i = 1
+            t = 1
+            head = '('
+            while i >0: 
+                if expr[t] == '(': i +=1
+                elif expr[t] == ')': i -=1
+                head = head + expr[t]
+                t +=1
+            if head == expr:
+                #remove parentheses on both sides
+                return(convert_rec(head[1:-1]))
+            # The expression can be divided into two - the head has parentheses, operater and then tail
+            else:
+                tail = ''
+                oper = ''
+                oper =  oper + expr[t]
+                tail = expr[t+1:]
+                return  oper + '' +  convert_rec(head) + convert_rec(tail)
+        # head is a single number and the operator in the middle of the two parts
         else:
-            while len(stack) > 0 and pres[e] < pres[stack[-1]]:
-                o = stack.pop()
-                res.append(o)
-            stack.append(e)
-
-    res.extend(stack)
-    res.reverse()
-    res = ' '.join(map(str, res))  #convert to string
-    return res
+            head = ''
+            oper = ''
+            head = head + expr[0]
+            oper = oper + expr[1]
+            tail = expr[2:]
+            return  oper + convert_rec(head) + convert_rec(tail)
 
 
 if __name__ == '__main__':
+    
+    i_expr = '( ( 1 * 2 ) + 3 )'
+    # Convert a infix to prefix
+    p_expr =infix_to_prefix(i_expr)
+    print(p_expr)
 
-    infix_input = ' ( ( ( 1 + 1 ) / 10 ) - ( 1 * 2 ) )'
-    prefix_input = infix_to_prefix(infix_input)
-    out =  prefix_eval(prefix_input)
-    print(out)
+    #calculate the prefix
+    result = prefix_eval(p_expr)
+    print(result)
